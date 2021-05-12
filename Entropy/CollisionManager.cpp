cd477925a287e97ec5c78a7c0e2d03a396d4e32e
David <bi-crosseur@hotmail.fr>
2021-05-13 00:20:33 +0200

    Update CollisionManager.cpp

--- Entropy/CollisionManager.cpp
+++ Entropy/CollisionManager.cpp
@@ -74,8 +74,8 @@ void CollisionManager::CheckCollisions(float delta)
                 Vector2 v2 = *other->GetVelocity();
                 Vector2 x1 = *entity->GetPosition();
                 Vector2 x2 = *other->GetPosition();
-                *entity->GetVelocity() = v1 - (x1 - x2) * (2.0f * m2) / (m1 + m2) * (v1 - v2).Dot(x1 - x2) / (x1 - x2).LengthSq();
-                *other->GetVelocity() = v2 - (x2 - x1) * (2.0f * m1) / (m1 + m2) * (v2 - v1).Dot(x2 - x1) / (x2 - x1).LengthSq();
+                *entity->GetVelocity() -= (x1 - x2) * (2.0f * m2) / (m1 + m2) * (v1 - v2).Dot(x1 - x2) / (x1 - x2).LengthSq();
+                *other->GetVelocity() -= (x2 - x1) * (2.0f * m1) / (m1 + m2) * (v2 - v1).Dot(x2 - x1) / (x2 - x1).LengthSq();
             }
             // Swept AABB collision detection
             else {
@@ -87,6 +87,7 @@ void CollisionManager::CheckCollisions(float delta)
                     *entity->GetPosition() += *entity->GetVelocity() * (delta * intersectFraction);
                     *other->GetPosition() += *other->GetVelocity() * (delta * intersectFraction);
 
+                    // TODO: Two-dimensional collision with two moving objects
                     relativeMotion.Normalize();
                     Vector2 tangent = relativeMotion.Tangent();
                     *entity->GetVelocity() = tangent * entity->GetVelocity()->Dot(tangent);
