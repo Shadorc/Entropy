ff82cfd3c8847a3440515900efcce34647a1d492
David <bi-crosseur@hotmail.fr>
2021-06-06 12:12:13 +0200

    Re-organize files

--- Entropy/MathUtil.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define FLOAT_INFINITY std::numeric_limits<float>::infinity()
-#define RAND_INT(max) rand() % max + 1
-#define INT(a) static_cast<int>(a)
-#define UINT(a) static_cast<unsigned int>(a)
-#define FLOAT(a) static_cast<float>(a)
-#define CLAMP(value, min, max) (value < min ? min : (value > max ? max : value))
-#define MIN(a, b) a < b ? a : b
--- Entropy/Precompiled.h
+++ Entropy/Precompiled.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define WIDTH 720
-#define HEIGHT 720
+constexpr int WIDTH = 720;
+constexpr int HEIGHT = 720;
 
 #include <math.h>
 #include <vector>
@@ -22,4 +22,12 @@
 #include "GravityComponent.h"
 #include "RigidBodyComponent.h"
 #include "Sandbox.h"
-#include "MathUtil.h"
+
+#define FLOAT_INFINITY std::numeric_limits<float>::infinity()
+#define RAND_INT(max) rand() % max + 1
+#define INT(a) static_cast<int>(a)
+#define UINT(a) static_cast<unsigned int>(a)
+#define FLOAT(a) static_cast<float>(a)
+#define CLAMP(value, min, max) (value < min ? min : (value > max ? max : value))
+#define MIN(a, b) a < b ? a : b
+#define MAX(a, b) a > b ? a : b
