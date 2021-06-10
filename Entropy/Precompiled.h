d440e80f4da25a6ba8765544f6e759f36b430d80
David <bi-crosseur@hotmail.fr>
2021-06-11 01:17:07 +0200

    Solve collisions multiple times to improve precision

--- Entropy/Precompiled.h
+++ Entropy/Precompiled.h
@@ -1,14 +1,15 @@
 #pragma once
 
-constexpr int WIDTH = 720;
-constexpr int HEIGHT = 720;
+constexpr unsigned int WIDTH = 720;
+constexpr unsigned int HEIGHT = 720;
 
 constexpr float DELTA_TIME = 1.0f / 60.0f;		// Target 60 FPS
 
-constexpr float PENETRATION_PERCENT = 0.8f;		// Penetration percentage to correct
+constexpr float PENETRATION_PERCENT = 0.4f;		// Penetration percentage to correct
 constexpr float PENETRATION_ALLOWANCE = 0.05f;	// Penetration allowance
+constexpr unsigned int COLLISIONS_SOLVER_ITERATIONS = 20;
 
-constexpr int CIRCLE_VERTICES = 24;
+constexpr unsigned int CIRCLE_VERTICES = 24;
 
 #ifdef _DEBUG
 #define ENTROPY_DEBUG
