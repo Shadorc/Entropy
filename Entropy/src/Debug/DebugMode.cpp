cd4872c25a4843b74068e122de96bb2ed5de512c
David <d.ceccarelli@outlook.fr>
2022-10-06 11:33:38 +0200

    Fix gitignore removing Debug folder sources

---
+++ Entropy/src/Debug/DebugMode.cpp
@@ -0,0 +1,44 @@
+#include "Precompiled.h"
+
+#ifdef ENTROPY_DEBUG
+DebugMode::DebugMode()
+	: m_Mask(0)
+	, m_PhysicStep(PhysicStep::NONE)
+{
+
+}
+
+void DebugMode::TogglePhysicStep()
+{
+	m_PhysicStep = (m_PhysicStep == PhysicStep::NONE) ? PhysicStep::PAUSE : PhysicStep::NONE;
+}
+
+void DebugMode::ContinuePhysicStep()
+{
+	m_PhysicStep = PhysicStep::CONTINUE;
+}
+
+bool DebugMode::ShouldContinuePhysicStep()
+{
+	if (m_PhysicStep == PhysicStep::NONE)
+	{
+		return true;
+	}
+	if (m_PhysicStep == PhysicStep::CONTINUE)
+	{
+		m_PhysicStep = PhysicStep::PAUSE;
+		return true;
+	}
+	return false;
+}
+
+void DebugMode::Toggle(DebugOption option)
+{
+	m_Mask ^= 1 << (int)option;
+}
+
+bool DebugMode::IsEnabled(DebugOption option) const
+{
+	return m_Mask & 1 << (int)option;
+}
+#endif // ENTROPY_DEBUG
